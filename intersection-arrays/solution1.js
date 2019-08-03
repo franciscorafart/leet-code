359d0b45695ffea273f8a889a9a9f0a198683304
Francisco <francisco.rafart@gmail.com>
2019-08-02 20:03:15 -0400

    Lint

--- intersection-arrays/solution1.js
+++ intersection-arrays/solution1.js
@@ -1,23 +1,24 @@
 // O(n)-solution
 var intersect = function(nums1, nums2) {
     let indexes = {}
-      let results = []
+    let results = [];
 
     for (let i=0; i<nums1.length; i++){
-      const num = nums1[i];
-      if(indexes[String(num)] === undefined){
-          indexes[String(num)] = [i];
-      } else {
-          indexes[String(num)].push(i)
-      }
+        const num = nums1[i];
+        if(indexes[String(num)] === undefined){
+            indexes[String(num)] = [i];
+        } else {
+            indexes[String(num)].push(i);
+        }
     }
 
-     for (let i=0;i<nums2.length; i++){
-         const num = nums2[i];
-         if (indexes[String(num)] && indexes[String(num)].length>0){
-             results.push(num);
-             indexes[String(num)].pop();
-         }
-     }
-      return results;
-  };
+    for (let i=0;i<nums2.length; i++){
+        const num = nums2[i];
+        if (indexes[String(num)] && indexes[String(num)].length>0){
+            results.push(num);
+            indexes[String(num)].pop();
+        }
+    }
+
+    return results;
+};
