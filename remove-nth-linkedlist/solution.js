ddb55cc142b9eda233e76c008dc9b1406c19748c
Francisco <francisco.rafart@gmail.com>
2019-08-03 13:59:00 -0400

    Solution remove nth linked list

---
+++ remove-nth-linkedlist/solution.js
@@ -0,0 +1,26 @@
+let removeHelper = (head, n, count) => {
+
+    if (n==count){ // removes first or only element
+        head = head.next
+    } else if (n<count-1){
+        head.next = removeHelper(head.next, n, count-1)
+    } else if (n==count-1){
+        if (head.next != null && head.next.next != null){
+            head.next = head.next.next
+        } else {
+            head.next = null
+        }
+    }
+
+    return head;
+}
+
+let removeNthFromEnd = function(head, n) {
+    let count = 1;
+    let thisHead = head;
+    while(thisHead.next != null){
+        thisHead = thisHead.next;
+        count++
+    }
+    return removeHelper(head, n, count);
+};
