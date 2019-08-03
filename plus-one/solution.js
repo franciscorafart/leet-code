8e97d6a12d451d405dcc17d4204f7eeaf334d9c0
Francisco <francisco.rafart@gmail.com>
2019-08-03 12:59:21 -0400

    Plus one solution

---
+++ plus-one/solution.js
@@ -0,0 +1,20 @@
+let plusOne = digits => {
+    let extra = 1;
+    for (let i=digits.length-1; i>=0; i--){
+        let add = digits[i]+extra;
+
+        if (add === 10){
+            digits[i] = 0;
+        } else {
+            digits[i]=add;
+            extra = 0;
+            break;
+        }
+    }
+
+    if (extra == 1){
+        digits.unshift(1)
+    }
+
+    return digits
+};
