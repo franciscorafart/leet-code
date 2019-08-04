cce886f0c8ea8bf8046a2079ad2d7b8bd8ec507e
Francisco <francisco.rafart@gmail.com>
2019-08-04 12:48:48 -0400

    Reversed linked list recursive solution

---
+++ reverse-linked-list/solution1.js
@@ -0,0 +1,19 @@
+let reverseListHelper = (head, previous) => {
+    let originalNext = null;
+    if (head.next)
+        originalNext = head.next;
+
+    head.next = previous
+
+    if(originalNext)
+        head = reverseListHelper(originalNext, head);
+
+    return head;
+}
+
+let reverseList = head => {
+    if (head)
+        return reverseListHelper(head, null);
+
+    return null
+};
