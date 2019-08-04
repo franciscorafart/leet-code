74fe2730756bbcfa523521027e352c4d6a812764
Francisco <francisco.rafart@gmail.com>
2019-08-04 13:09:27 -0400

    Iterative solution

---
+++ reverse-linked-list/solution2.js
@@ -0,0 +1,20 @@
+// Iteration
+let reverseList = head => {
+    if (head){
+        let previous = null;
+        let following = null;
+
+        while(head.next){
+            following = head.next;
+            head.next=previous;
+            previous = head;
+            head = following;
+        }
+
+        head.next = previous;
+
+        return head;
+    }
+
+    return null
+}
