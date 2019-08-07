f4c9830a8df2bde8f18434221cf9c17ba28ee565
Francisco <francisco.rafart@gmail.com>
2019-08-06 20:32:15 -0400

    Valid palindrome solution

---
+++ valid-palindrome/solution.js
@@ -0,0 +1,23 @@
+let isAlphaNum = ch => {
+    //65-90 or 48 to 57
+    const charCode = ch.charCodeAt(0);
+    if((charCode <=122 && charCode >= 97) || (charCode >= 48 && charCode <= 57))
+        return true;
+    return false;
+}
+
+let isPalindrome = s => {
+    if(s.length>0){
+        let sClean = s.toLowerCase().split('').filter(ch => isAlphaNum(ch))
+        let i = 0;
+        let j = sClean.length-1;
+
+        while(i<j){
+            if(sClean[i] != sClean[j])
+              return false
+            i++
+            j--
+        }
+    }
+    return true;
+};
