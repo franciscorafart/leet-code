1f93d0c47d8350fc1bbad04bf4f491b341f47c72
Francisco <francisco.rafart@gmail.com>
2019-08-16 16:59:32 -0400

    First and last position solution

---
+++ first-last-position/solution.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// Test cases:
+// [] 4 => [-1, -1]
+// [1,2,3,4,7,7,7,7,7,7,7,8,9,10,11,12,14]  / 7
+// [1,2,3,4,5,6,7] / 6
+// [1,2,3,4,5,6,7] / 1
+
+let binaryS = (nums, target, min, max) => {
+
+    if (min == null || max == null){
+        min=0;
+        max=nums.length-1;
+    }
+
+    if (min>max)
+        return null
+
+    const midIdx = min + Math.ceil((max-min)/2)
+
+    if (nums[midIdx] === target)
+        return midIdx
+
+    if(nums[midIdx]>target){
+        return binaryS(nums, target, min, midIdx-1);
+    }
+
+    return binaryS(nums, target, midIdx+1, max);
+}
+
+var searchRange = function(nums, target) {
+    if (Array.isArray(nums) || target.isInteger()){
+        const idx = binaryS(nums,target)
+
+        if (idx !== null){
+            let left = idx;
+            let right = idx;
+
+            if (right<nums.length-1){
+                while(nums[right+1] === target){
+                    right++
+                }
+            }
+            if (left>0){
+                while(nums[left-1] === target){
+                    left --
+                }
+            }
+
+            return [left, right]
+        }
+    }
+
+    return [-1,-1]
+};
