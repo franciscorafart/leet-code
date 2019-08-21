e8d0b6c720774c3dab8fdaf12d866b0b77f18ec8
Francisco <francisco.rafart@gmail.com>
2019-08-21 19:18:03 -0400

    Lemonade change solution

---
+++ lemonade-change/solution.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+
+var lemonadeChange = function(bills) {
+    const keyVal = [[20, 0], [10, 0], [5, 0]]
+    const register = new Map(keyVal);
+
+    for (let i=0;i<bills.length; i++){
+        const pay = bills[i];
+        register.set(pay, register.get(pay)+1);
+
+        let changeDue = pay-5;
+
+        if (changeDue === 0)
+            continue
+
+        for (let [bill, amount] of register){
+            while (amount>0 && bill<=changeDue){
+                changeDue-=bill
+                amount--
+            }
+            register.set(bill, amount)
+        }
+        if (changeDue != 0)
+            return false
+    }
+    return true
+};
