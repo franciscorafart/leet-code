95861979a4a7dcc1bc308724c95f2cc3b84b62dd
Francisco <francisco.rafart@gmail.com>
2019-09-24 10:17:37 -0400

    Solution 1

---
+++ bin-tree-paths/solution.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+
+// Test cases
+// [1,2,3,null,5] -> ["1->2->5","1->3"]
+// [] -> []
+
+const isLeaf = node => {
+    if (node.left || node.right)
+        return false;
+
+    return true;
+}
+
+const traverseDepth = (node, path, paths) => {
+    console.log('currentPath', path)
+    if (node.left) {
+        const leftPath = path.concat([node.left.val]);
+        if (isLeaf(node.left)){
+            paths.push(leftPath);
+        }
+        traverseDepth(node.left, leftPath, paths);
+    }
+    if (node.right){
+        const rightPath = path.concat([node.right.val]);
+        if (isLeaf(node.right)){
+            paths.push(rightPath);
+        }
+        traverseDepth(node.right, rightPath, paths);
+    }
+};
+
+var binaryTreePaths = function(root) {
+    let paths = [];
+    if (root){
+        traverseDepth(root, [root.val], paths);
+    }
+
+    const result = paths.map(path => path.join("->"))
+
+    return result;
+};
