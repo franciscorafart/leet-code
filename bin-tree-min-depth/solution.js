d1f10626ab42207fbe5fa147fb79b231874ebe2d
Francisco <francisco.rafart@gmail.com>
2019-09-29 15:47:32 -0400

    Binary tree min depth solution

---
+++ bin-tree-min-depth/solution.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+// Test cases
+// [3,9,20,null,null,15,7] -> 2
+// [] -> 0
+// [0] -> 1
+// [3,9,20,null,11,15,7,6,9] -> 3
+
+let isLeaf = node => node && !node.right && !node.left
+
+var minDepth = function(root) {
+    if(!root)
+        return 0;
+
+    let queue = [[root, 0]];
+    let depth = 0;
+
+    while (queue.length>0){
+        let [node, localDepth] = queue.shift();
+        depth=localDepth+1;
+
+        if (isLeaf(node))
+            return depth;
+
+        if (node.left)
+            queue.push([node.left, depth]);
+
+        if (node.right)
+            queue.push([node.right, depth]);
+    }
+
+    return depth;
+};
