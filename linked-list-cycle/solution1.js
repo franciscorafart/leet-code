dfce5fe16c87e7e296c56d38df0aba962d745c2e
Francisco <francisco.rafart@gmail.com>
2019-10-26 10:56:05 -0400

    Solution for linked list cycle

---
+++ linked-list-cycle/solution1.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+// Test cases
+// head = [3,2,0,-4], pos = 1 => true
+// head = [3,2,0,-4], pos = 2 => true
+// head = [3,2,0,-4], pos = -1 => false
+// head = [1,2], pos = 0 => true
+// head = [1], pos=-1 => true
+// head = [], pos = -1 => false
+
+var hasCycle = function(head) {
+    let visited = false;
+    if (head){
+        const visitedMemo = new Map();
+        while (!visited && head.next != null){
+            if (visitedMemo.get(head)){
+                visited=true;
+            } else {
+                visitedMemo.set(head, true);
+                head = head.next;
+            }
+        }
+    }
+
+    return visited
+};
