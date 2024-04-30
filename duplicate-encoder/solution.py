0033342eb33178fd6909735d172c0fe400b0b9e3
Francisco Rafart <francisco.rafart@gmail.com>
2024-04-29 21:06:17 -0400

    Duplicate encoder 6kyu code wars

---
+++ duplicate-encoder/solution.py
@@ -0,0 +1,23 @@
+# The goal of this exercise is to convert a string to a new string where each character in the 
+# new string is "(" if that character appears only once in the original string, or ")" if that
+# character appears more than once in the original string. Ignore capitalization when determining 
+# if a character is a duplicate.
+
+# Examples
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))((" 
+# Notes
+# Assertion messages may be unclear about what they display in some languages. 
+# If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+def duplicate_encode(word):
+    w = word.lower()
+    count = dict()
+    for ch in w:
+        current = count.get(ch, None)
+        count[ch] = current + 1 if current is not None else 1
+    
+    return "".join(["(" if (count[ch] == 1) else ")" for ch in w])
+            
