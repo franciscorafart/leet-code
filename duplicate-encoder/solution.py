0910c71ec4f497cf52bd08dd3c581ea5cfa18828
Francisco Rafart <francisco.rafart@gmail.com>
2024-04-29 21:07:32 -0400

    Add codewars reference

--- duplicate-encoder/solution.py
+++ duplicate-encoder/solution.py
@@ -1,3 +1,7 @@
+# Code Wars - Duplicate Encoder
+# https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+# Difficulty: 6 kyu
+
 # The goal of this exercise is to convert a string to a new string where each character in the 
 # new string is "(" if that character appears only once in the original string, or ")" if that
 # character appears more than once in the original string. Ignore capitalization when determining 
